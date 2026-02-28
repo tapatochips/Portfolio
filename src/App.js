f163026ed490db41664959bb43de580ef2fc3f00
Daniel Richmond <richmonddaniel79@gmail.com>
2026-02-27 18:02:02 -0600

    \

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
